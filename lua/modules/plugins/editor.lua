8b878a9a87fc098e9e6889e51c9f365b014034cd
Joel Abreu <joelabreurojas@gmail.com>
2024-05-10 02:55:34 -0400

    feat(codeium.vim): free `copilot` alternative

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -1,5 +1,24 @@
 local editor = {}
 
+editor["Exafunction/codeium.vim"] = {
+	event = "BufEnter",
+	config = function()
+		-- Change '<C-g>' here to any keycode you like.
+		vim.keymap.set("i", "<C-g>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<M-]>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<M-[>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<C-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true, silent = true })
+	end,
+}
+
 editor["olimorris/persisted.nvim"] = {
 	lazy = true,
 	cmd = {
