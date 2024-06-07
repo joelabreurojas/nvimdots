b6d7e760544c41da2a0c37c655a94be94abcb364
Joel Abreu <joelabreurojas@gmail.com>
2024-06-07 05:46:48 -0400

    refactor(editor): change keybinds for codeium

--- lua/modules/plugins/editor.lua
+++ lua/modules/plugins/editor.lua
@@ -4,13 +4,10 @@ editor["Exafunction/codeium.vim"] = {
 	event = "BufEnter",
 	config = function()
 		-- Change '<C-g>' here to any keycode you like.
-		vim.keymap.set("i", "<C-g>", function()
-			return vim.fn["codeium#Accept"]()
-		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<M-]>", function()
+		vim.keymap.set("i", "<C-]>", function()
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true, silent = true })
-		vim.keymap.set("i", "<M-[>", function()
+		vim.keymap.set("i", "<C-[>", function()
 			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true, silent = true })
 		vim.keymap.set("i", "<C-x>", function()
